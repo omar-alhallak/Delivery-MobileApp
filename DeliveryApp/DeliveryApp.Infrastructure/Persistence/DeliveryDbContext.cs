733c5c018459911d68a594d7a855cb5d35c0b226
Omar alhallak <omaralhallak0998976714@gmail.com>
2026-01-20 00:48:53 +0300

    Bild project

---
+++ DeliveryApp/DeliveryApp.Infrastructure/Persistence/DeliveryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Infrastructure.Persistence
+{
+    public class DeliveryDbContext :DbContext
+    {
+        public DeliveryDbContext(DbContextOptions<DeliveryDbContext> options) : base(options) { }
+    }
+}
