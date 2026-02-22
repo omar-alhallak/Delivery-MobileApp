a7e4908f53aaf561bacb9f4a51d6136c87201d49
Omar alhallak <omaralhallak0998976714@gmail.com>
2026-02-22 13:43:49 +0300

    Fix Bug

--- Delivery_MobailAPP/DeliveryApp.Application/Abstractions/Persistence/Class1.cs
+++ Delivery_MobailAPP/DeliveryApp.Application/Abstractions/Persistence/Class1.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DeliveryApp.Domain.Entities;
 
 namespace DeliveryApp.Application.Abstractions.Persistence
 {
