0e5d5a7ea521b3ce630e4c389e57faf5bb826de7
LOQ <LOQ@DESKTOP-VKC50EI>
2026-04-02 06:01:40 -0900

    add configuration

---
+++ Delivery_MobailAPP/DeliveryApp.Infrastructure/Configuration/EngagementsConfiguration/Notification.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeliveryApp.Domain.Entities.Engagements;
+using DeliveryApp.Domain.Entities.Identity;
+using DeliveryApp.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DeliveryApp.Infrastructure.Configuration.EngagementsConfiguration
+{
+    public sealed class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            builder.ToTable("Notifications");
+
+            // -------------------------
+            //            Key
+            // -------------------------
+
+            builder.HasKey(x => x.ID);
+
+            builder.Property(x => x.ID)
+                .HasConversion(
+                    id => id.Value,
+                    value => StrongID<NotificationTag>.From(value))
+                .ValueGeneratedNever();
+
+            // -------------------------
+            //      restrictions
+            // -------------------------
+
+            builder.Property(x => x.Body)
+                .HasMaxLength(500)
+                .IsUnicode(false)
+                .IsRequired(false);
+
+            builder.Property(x => x.Title)
+                .HasMaxLength(150)
+                .IsUnicode(true)
+                .IsRequired(false);
+
+
+            builder.Property(x => x.IsRead)
+                .IsRequired();
+
+            // -------------------------
+            //          Enum
+            // -------------------------
+
+            builder.Property(x => x.RelatedEntityType)
+                .HasConversion<int>() 
+                .IsRequired(false);
+
+            // -------------------------
+            //       ForeignKey
+            // -------------------------
+
+            builder.Property(x => x.UserID)
+               .HasConversion(
+                   id => id.Value,
+                   value => StrongID<UserTag>.From(value))
+               .IsRequired();
+
+            builder.HasOne<User>()
+               .WithMany()
+               .HasForeignKey(x => x.UserID)
+               .IsRequired(false)
+               .OnDelete(DeleteBehavior.Restrict);
+
+
+
+            builder.Property(x => x.RelatedEntityID)
+              .HasConversion(
+                   id => id,
+                   value => value
+               )
+              .IsRequired(false);
+
+            builder.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(x => x.RelatedEntityID)
+                .IsRequired(false)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // -------------------------
+            //           Dates
+            // -------------------------
+
+            builder.Property(x => x.CreatedAt)
+                .HasColumnType("datetimeoffset")
+                .IsRequired();
+
+            builder.Property(x => x.ReadAt)
+                .HasColumnType("datetimeoffset")
+                .IsRequired(false);
+        }
+    }
+}
