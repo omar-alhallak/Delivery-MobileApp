177ec5f38f2d7b5b34d68296fbc17f92aca2b8ea
LOQ <LOQ@DESKTOP-VKC50EI>
2026-04-02 06:03:20 -0900

    edit

--- Delivery_MobailAPP/DeliveryApp.Infrastructure/Configuration/EngagementsConfiguration/Rating.cs
+++ Delivery_MobailAPP/DeliveryApp.Infrastructure/Configuration/EngagementsConfiguration/Rating.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DeliveryApp.Domain.Entities.Engagements;
+﻿using DeliveryApp.Domain.Entities.Engagements;
 using DeliveryApp.Domain.Entities.Identity;
 using DeliveryApp.Domain.Entities.Orders;
 using DeliveryApp.Domain.ValueObjects;
