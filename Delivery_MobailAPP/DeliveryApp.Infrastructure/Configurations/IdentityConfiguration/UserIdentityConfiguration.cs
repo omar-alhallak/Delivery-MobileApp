f1086f874b7ec230e1f8d428c0db1028c6a294ea
Bilal Abdel Nabi <bilalabdelnabi5@gmail.com>
2026-04-10 17:28:51 +0300

    Done

--- Delivery_MobailAPP/DeliveryApp.Infrastructure/Configurations/IdentityConfiguration/UserIdentityConfiguration.cs
+++ Delivery_MobailAPP/DeliveryApp.Infrastructure/Configurations/IdentityConfiguration/UserIdentityConfiguration.cs
@@ -1,12 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using DeliveryApp.Domain.Entities.Identity;
+using DeliveryApp.Domain.ValueObjects;
+using DeliveryApp.Domain.Enums.IdentityEnums;
 
-namespace DeliveryApp.Infrastructure.Configurations.Identity
+namespace DeliveryApp.Infrastructure.Configurations.IdentityConfiguration
 {
-    internal class UserIdentityConfiguration
+    public sealed class UserIdentityConfiguration : IEntityTypeConfiguration<UserIdentity>
     {
+        public void Configure(EntityTypeBuilder<UserIdentity> builder)
+        {
+            builder.ToTable("UserIdentities", "identity");
+
+            // -------------------------
+            //            Key
+            // -------------------------
+
+            builder.HasKey(x => x.ID);
+
+            builder.Property(x => x.ID)
+                .HasConversion(
+                    id => id.Value,
+                    value => StrongID<UserIdentityTag>.From(value))
+                .ValueGeneratedNever();
+
+            // -------------------------
+            //         Relations
+            // -------------------------
+
+            builder.Property(x => x.UserID)
+                .HasConversion(
+                    id => id.Value,
+                    value => StrongID<UserTag>.From(value))
+                .IsRequired();
+
+            builder.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(x => x.UserID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // -------------------------
+            //           Enums
+            // -------------------------
+
+            builder.Property(x => x.Provider)
+                .HasConversion<byte>()
+                .IsRequired();
+
+            // -------------------------
+            //           Fields
+            // -------------------------
+
+            builder.Property(x => x.ProviderUserId)
+                .HasMaxLength(128)
+                .IsUnicode(false)
+                .IsRequired(false);
+
+            builder.Property(x => x.PasswordHash)
+                .HasMaxLength(300)
+                .IsUnicode(false)
+                .IsRequired(false);
+
+            builder.Property(x => x.CreatedAt)
+                .HasColumnType("datetimeoffset")
+                .IsRequired();
+
+            // -------------------------
+            //          Indexes
+            // -------------------------
+
+            // منع تكرار نفس الحساب من نفس المزود
+            builder.HasIndex(x => new { x.Provider, x.ProviderUserId })
+                .IsUnique()
+                .HasFilter("[ProviderUserId] IS NOT NULL");
+
+            // ممكن مستخدم عنده مزود واحد مرة وحدة
+            builder.HasIndex(x => new { x.UserID, x.Provider })
+                .IsUnique();
+        }
     }
 }
